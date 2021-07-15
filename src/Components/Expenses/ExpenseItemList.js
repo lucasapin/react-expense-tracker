1ab7d7429bf2521a7b02c3fdcaaaaf95d0e117f2
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-07-15 12:56:59 -0400

    Refactor project folders

--- src/Components/Expenses/ExpenseItemList.js
+++ src/Components/Expenses/ExpenseItemList.js
@@ -1,9 +1,10 @@
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseItemList.css";
+import Card from "./Card"
 
 function ExpenseItemList(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -24,7 +25,7 @@ function ExpenseItemList(props) {
         amount={props.expenses[3].amount}
         date={props.expenses[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
