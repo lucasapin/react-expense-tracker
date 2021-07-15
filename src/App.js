92ee357a1b72a07024a7ccf50b48cd10a1bff670
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-07-15 12:21:37 -0400

    Create card component and refactor some styles into the new card.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ExpenseItemList from './Components/ExpenseItemList';
 
 function App() {
   const expenses = [
-    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 3, 12) },
+    { title: "Car Insurance", amount: 300.67, date: new Date(2021, 3, 12) },
     { title: "Toilet Paper", amount: 24.88, date: new Date(2021, 4, 28) },
     { title: "New Tv", amount: 854.35, date: new Date(2021, 5, 29) },
     { title: "New Desk (Wooden)", amount: 742.22, date: new Date(2021, 7, 12) },
