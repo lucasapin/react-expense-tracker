825c09da1c629676219438f0c7c1c040a3720f56
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-07-20 10:49:04 -0400

    Change form style

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -4,20 +4,20 @@ import './ExpenseForm.css';
 const ExpenseForm = () => {
     return (
         <form >
-            <div className="new_expense__controls">
-                <div clasName="new_expense__control">
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
                     <label>Title</label>
                     <input type='text' />
                 </div>
-                <div clasName="new_expense__control">
+                <div className='new-expense__control'>
                     <label>Amount</label>
                     <input type='number' min="0.01" step="0.01" />
                 </div>
-                <div clasName="new_expense__control">
+                <div className='new-expense__control'>
                     <label>Date</label>
                     <input type='date' min="2019-01-01" max="2022-12-31" />
                 </div>
-                <div className="new_expense__actions">
+                <div className='new-expense__actions'>
                     <button type="submit">Add Expense</button>
                 </div>
             </div>
