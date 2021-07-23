363980c5665209c00991fa9f97c5a0ebb408e4ab
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-07-22 22:23:05 -0400

    Create state in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItemList from './Components/Expenses/ExpenseItemList';
 import NewExpense from './Components/NewExpense/NewExpense';
 
 
+const dummy_expenses = [
+  { title: "Car Insurance", amount: 300.67, date: new Date(2021, 3, 12) },
+  { title: "Toilet Paper", amount: 24.88, date: new Date(2021, 4, 28) },
+  { title: "New Tv", amount: 854.35, date: new Date(2021, 5, 29) },
+  { title: "New Desk (Wooden)", amount: 742.22, date: new Date(2021, 7, 12) },
+];
 function App() {
-  const expenses = [
-    { title: "Car Insurance", amount: 300.67, date: new Date(2021, 3, 12) },
-    { title: "Toilet Paper", amount: 24.88, date: new Date(2021, 4, 28) },
-    { title: "New Tv", amount: 854.35, date: new Date(2021, 5, 29) },
-    { title: "New Desk (Wooden)", amount: 742.22, date: new Date(2021, 7, 12) },
-  ];
+  const [expenses, setExpenses] = useState(dummy_expenses)
 
   const addExpenseHandler = (expense) => {
-    console.log('Printed in App.js')
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expenses, ...prevExpenses];
+    })
   }
   return (
     <div>
